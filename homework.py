87ff00cdab3a4bd5c1d0105122eacff9ca55c67c
AnnSonrisa <53565314+AnnSonrisa@users.noreply.github.com>
2021-09-20 16:20:12 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,15 +1,14 @@
 import datetime as dt
 
-FORMAT_OF_DATE = '%d.%m.%y'
-CASH_BALANCE = '''На сегодня осталось {balance}
-{currency_name}'''
-CASH_DEBT = '''Денег нет, держись: твой долг -
-            {today_remains_currency} {currency_name}'''
-CASH_NO = 'Денег нет, держись'
-EXCEPTION = 'Currency не существует в словаре'
-CALORIES_BALANCE = '''Сегодня можно съесть что-нибудь ещё,
-но с общей калорийностью не более {int(today_remains)} кКал'''
-CALORIES_STOP = 'Хватит есть!'
+NO_CASH = 'Денег нет, держись'
+CASH_CURRENCY = '{today_remains_currency} {currency_name}'
+CASH_REMAINS = 'На сегодня осталось ' + CASH_CURRENCY
+CASH_CREDIT = 'Денег нет, держись: твой долг - ' + CASH_CURRENCY
+FORMAT_OF_DATE = '%d.%m.%Y'
+CALORIES_LIMIT = 'с общей калорийностью не более {today_remains} кКал'
+CALORIES_REMAINS = 'Сегодня можно съесть что-нибудь ещё, но ' + CALORIES_LIMIT
+NO_CALORIES = 'Хватит есть!'
+
 
 
 class Record:
@@ -34,20 +33,19 @@ class Calculator:
     def get_today_stats(self) -> float:
         """ Подсчет денег/калорий, потраченных сегодня """
         today = dt.date.today()
-        today_amount = sum[(record.amount
-                           for record in self.records
-                            if record.date == today)]
+        today_amount = sum([record.amount
+                            for record in self.records
+                            if record.date == today])
         return today_amount
 
     def get_week_stats(self) -> float:
         """ Подсчет денег/калорий, потраченных за неделю """
         now = dt.date.today()
         then = now - dt.timedelta(days=7)
-        week_amount = 0
-        week_amount = sum[(record.amount
-                           for record in self.records
-                           if then <= record.date <= now)]
-        return float(week_amount)
+        week_amount = float(sum([record.amount
+                            for record in self.records
+                            if then <= record.date <= now]))
+        return week_amount
 
     def get_today_remained(self) -> float:
         """ Подсчет денег/калорий, которые можно потратить/получить сегодня """
@@ -61,8 +59,8 @@ class CaloriesCalculator(Calculator):
         """ Подсчет калорий, которые можно получить сегодня """
         today_remains = self.get_today_remained()
         if today_remains > 0:
-            return CALORIES_BALANCE.format(today_remains=today_remains)
-        return CALORIES_STOP
+            return CALORIES_REMAINS.format(today_remains=int(today_remains))
+        return NO_CALORIES
 
 
 class CashCalculator(Calculator):
@@ -73,21 +71,24 @@ class CashCalculator(Calculator):
     def get_today_cash_remained(self, currency) -> str:
         """ Подсчет денег, которые можно потратить сегодня """
         CURRENCIES = {'eur': ('Euro', CashCalculator.EURO_RATE),
+
                       'usd': ('USD', CashCalculator.USD_RATE),
+
                       'rub': ('руб', CashCalculator.RUB_RATE)}
         today_remains = self.get_today_remained()
         try:
             currency_name, currency_rate = CURRENCIES[currency]
         except Exception as exc:
-            raise exc(EXCEPTION)
-
+            raise exc('Currency нет в словаре')
         today_remains_currency = abs(round(today_remains / currency_rate, 2))
         if not today_remains:
-            return CASH_NO
+            return NO_CASH
         if today_remains > 0:
-            return CASH_BALANCE.format(
-                balance=today_remains_currency,
+            return CASH_REMAINS.format(
+                today_remains_currency=today_remains_currency,
                 currency_name=currency_name)
-        return CASH_DEBT.format(
+        return CASH_CREDIT.format(
             today_remains_currency=today_remains_currency,
             currency_name=currency_name)
+
+
